ff84b382d863d9ea7d27574e7a7c3ef07b19b07f
LCT <136698906+lctproj@users.noreply.github.com>
2024-04-06 02:59:34 +0800

    try

--- src/components/CartItemCard.jsx
+++ src/components/CartItemCard.jsx
@@ -1,105 +1,29 @@
-import react, {useState, useEffect} from 'react';
+import react, {useState} from 'react';
 import { FiPlusCircle, FiMinusCircle} from "react-icons/fi";
 import { BsTrash } from "react-icons/bs";
 import "../styles/shopping_cart_item.css";
 
-export function CartItemCard ({product}){
+export function CartItemCard ({product, onDelete, onPlusOne, onMinusOne}}){
 
     const [purchased, setPurchased] = useState(product.purchased);
     const [subtotal, setSubtotal] = useState((product.purchased * product.item.price).toFixed(2));
 
     const user_id = "001";
 
-    const QuantityPlusOne = async () =>{
-        if(purchased >= 1){
-            try{
-                const params = {
-                    "user_id": user_id,
-                    "item_id": product.item.item_id
-                };        
+    const QuantityPlusOne = ()=> {
+        onPlusOne(product.item.item_id);
 
-                const response = await fetch('http://localhost:3001/quantity-plus-one',{
-                    method: 'POST',
-                    headers : {
-                        'Content-Type':'application/json'
-                    },
-                    body : JSON.stringify(params)
-                });
-
-             
-                
-                const result = await response.json();
-                console.log(result);
-                
-                if(response.ok){
-                    setPurchased(result.purchased);
-                    setSubtotal(result.subtotal);
-                } else {
-                    if(response.status === 400){
-                        alert(`${result.message}`);
-                    }
-                }
-            }catch(err) {console.log(err)}
-        }
     }
 
-    const QuantityMinusOne = async () =>{
-        if(purchased >= 1){
-            try{
-                const params = {
-                    "user_id": user_id,
-                    "item_id": product.item.item_id
-                };        
-
-                const response = await fetch('http://localhost:3001/quantity-minus-one',{
-                    method: 'POST',
-                    headers : {
-                        'Content-Type':'application/json'
-                    },
-                    body : JSON.stringify(params)
-                });
-
-                if (!response.ok) {
-                    throw new Error('Error updating quantity of shopping cart item');
-                  }
-                
-                const result = await response.json();
-
-                setPurchased(result.purchased);
-                setSubtotal(result.subtotal);
-            }catch(err) {console.log(err)}
-        }
+    const QuantityMinusOne = ()=> {
+        onMinusOne(product.item.item_id);
+        
     }
-  
-    const DeleteFromCart =  async() =>{
-        if(confirm(`Are you sure you want to delete ${product.item.name} from cart? \n(This action is irreversible)`)){
-            if(purchased > 0){
-                try{
-                    const data = {
-                        "user_id": user_id,
-                        "item_id": product.item.item_id
-                    };        
 
-                    const response = await fetch('http://localhost:3001/delete-item',{
-                        method: 'DELETE',
-                        headers : {
-                            'Content-Type':'application/json'
-                        },
-                        body : JSON.stringify(data)
-                    });
-
-
-                    if (!response.ok) {
-                        throw new Error('Error deleting shopping cart item');
-                    }
-                    
-                    
-                    
-                }catch(err) {console.log(err)}
-        }
-    }
-}
- 
+    const DeleteFromCart = () => {
+        onDelete(product.item.item_id);
+    };
+    
     return (
         <div className="row card-item hover-shadow rounded-2 my-1">
             <div className="col col-md-2">
--- src/components/ShoppingCartPage.jsx
+++ src/components/ShoppingCartPage.jsx
@@ -1,53 +1,143 @@
-import react, {useState, useEffect} from 'react';
+import react, {useState, useEffect, useCallback} from 'react';
 import {CartItemCard} from './CartItemCard';
 export function ShoppingCartPage(){
     const [total, setTotal] = useState(0);
     const [fetched, setFetched] = useState([]);
 
     const user_id = "001"; // delete this line after setting up account management
+    const fetchData = useCallback(async()=>{
+      try {
+        const data = {
+          "user_id" : user_id
+        };
+
+        const response = await fetch('http://localhost:3001/all-cart-items',{
+          method : 'POST',
+          headers:{
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        });
+
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+
+        const items = await response.json();
+        /*
+        items=
+          {         
+                items: user_cart,
+                total: total,
+          }
+        */
+        setFetched(items.items);
+        setTotal(items.total); 
+        console.log('Total updated to:', items.total);
+      } catch (err) {
+        console.error(err);
+      }
+    },[user_id]);
 
     useEffect(() => {
-        async function fetchData() {
-          try {
-            const data = {
-              "user_id" : user_id
-            };
 
-            const response = await fetch('http://localhost:3001/all-cart-items',{
-              method : 'POST',
-              headers:{
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(data)
+        fetchData();    
+    },[fetchData]);
+
+    const QuantityPlusOne = async (item_id) =>{
+          try{
+              const params = {
+                  "user_id": user_id,
+                  "item_id": item_id
+              };        
+
+              const response = await fetch('http://localhost:3001/quantity-plus-one',{
+                  method: 'POST',
+                  headers : {
+                      'Content-Type':'application/json'
+                  },
+                  body : JSON.stringify(params)
+              });
+
+           
+              
+              const result = await response.json();
+              console.log(result);
+              
+              if(response.ok){
+                  setPurchased(result.purchased);
+                  setSubtotal(result.subtotal);
+              } else {
+                  if(response.status === 400){
+                      alert(`${result.message}`);
+                  }
+              }
+          }catch(err) {console.log(err)}
+      }
+  
+
+  const QuantityMinusOne = async (item_id) =>{
+    
+        try{
+            const params = {
+                "user_id": user_id,
+                "item_id": item_id
+            };        
+
+            const response = await fetch('http://localhost:3001/quantity-minus-one',{
+                method: 'POST',
+                headers : {
+                    'Content-Type':'application/json'
+                },
+                body : JSON.stringify(params)
             });
 
             if (!response.ok) {
-              throw new Error(`Error: ${response.status} ${response.statusText}`);
-            }
-
-            const items = await response.json();
-            /*
-            items=
-              {         
-                    items: user_cart,
-                    total: total,
+                throw new Error('Error updating quantity of shopping cart item');
               }
-            */
-            setFetched(items.items);
-            setTotal(items.total); 
-          } catch (err) {
-            console.error(err);
-          }
-        }
+            
+            const result = await response.json();
 
-        fetchData();    
-    },[]);
+            setPurchased(result.purchased);
+            setSubtotal(result.subtotal);
+        }catch(err) {console.log(err)}
+    }
+
+
+    const DeleteFromCart =  async(item_id) =>{
+      if(confirm(`Are you sure you want to delete ${product.item.name} from cart? \n(This action is irreversible)`)){
+              try{
+                  const data = {
+                      "user_id": user_id,
+                      "item_id": item_id
+                  };        
+
+                  const response = await fetch('http://localhost:3001/delete-item',{
+                      method: 'DELETE',
+                      headers : {
+                          'Content-Type':'application/json'
+                      },
+                      body : JSON.stringify(data)
+                  });
+
+
+                  if (!response.ok) {
+                      throw new Error('Error deleting shopping cart item');
+                  }
+                  
+                  await fetchData();
+                  
+              }catch(err) {console.log(err)}
+          }
+      }
+    
 
     return (
         <>
             <h1>Shopping Cart</h1>
             {fetched.map((item) => (
-                <CartItemCard key={item.item.item_id} product={item} />
+                <CartItemCard key={item.item.item_id} product={item} onDelete = {DeleteFromCart} onPlusOne={QuantityPlusOne} onMinusOne={QuantityMinusOne} 
+                />
             ))}            
             <div className="text-end" style={{ fontSize: '1.25rem'} }><p>Total Price &#58; &#36;  {total}</p></div>
             <div className="text-end" ><button className="btn btn-info hover">Checkout</button></div>
