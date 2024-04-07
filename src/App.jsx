8fdbb99c3986de5ba3380d084866346cb74e1b25
LCT <136698906+lctproj@users.noreply.github.com>
2024-04-07 17:05:54 +0800

    trying sth new

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css';
-import { ShoppingCartPage } from './components/ShoppingCartPage';
+//import { ShoppingCartPage } from './components/ShoppingCartPage';
+import {HeaderBar} from './components/HeaderBar';
+import {Home} from './components/Home';
+import {SignUpForm} from './components/SignUpForm';
+import {LoginPage} from './components/LoginPage';
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+ 
 
 function App() {
   return(
-    <div>
-      <ShoppingCartPage />
-    </div>
+    <BrowserRouter>
+      <div>
+        <HeaderBar/>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/register" element={<SignUpForm />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+export default function Home (){
+    return(
+        <>
+            <h1>Welcome Home</h1>
+            <button type="button" className="btn btn-primary">To cart</button>
+        </>
+        
+    )
+}
